0c115ca0b68bfb87a9c3e159fdadaf6f1633475c
AndyG <anderson.garron@gmail.com>
2018-10-07 13:24:00 -0700

    basic melee w/ particles

---
+++ Assets/Scripts/Misc/Lifetime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lifetime : MonoBehaviour
+{
+
+    [SerializeField]
+    private float lifetime = 5f;
+
+    float timeAlive = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeAlive >= lifetime) {
+            Destroy(gameObject);
+        }
+        timeAlive += Time.deltaTime;
+    }
+}
